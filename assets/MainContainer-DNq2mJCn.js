d10a87f5bc785c4148af6e75528b3ea69ccd8a22
1OmarTarek1 <cr7omartarek@gmail.com>
2025-12-08 13:52:13 +0200

    Updates

---
+++ assets/MainContainer-DNq2mJCn.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-ui-8aapSjLi.js";import"./index-Bob8hhO9.js";const a=n=>r.jsx("div",{className:"MainContainer",children:n.children});export{a as M};
--- assets/MainContainer-FjmAJ0nv.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./vendor-ui-8aapSjLi.js";import"./index-BO56jMQy.js";const a=n=>r.jsx("div",{className:"MainContainer",children:n.children});export{a as M};
